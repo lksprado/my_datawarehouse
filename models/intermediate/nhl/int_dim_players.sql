8195eccd355df0cfd0571c23245514683e3718ff
lksprado <lks-prado@live.com>
2026-01-26 07:50:46 -0300

    fix dedup dim players

--- models/intermediate/nhl/int_dim_players.sql
+++ models/intermediate/nhl/int_dim_players.sql
@@ -7,10 +7,7 @@
 
 with source as (
     select
-        *,
-        row_number() over (
-            partition by player_id
-        ) as rn
+        *
     from {{ ref('stg_all_players') }}
     where season_type = 'regular'
 ),
@@ -31,6 +28,9 @@ players as (
 
 player_info as (
     select
+        row_number() over (
+            partition by t1.player_id
+        ) as rn,
         t1.player_id,
         t1.player_firstname,
         t1.player_lastname,
@@ -56,7 +56,33 @@ player_info as (
     from source as t1
     left join players as t2
         on t1.player_id = t2.player_id
-    where rn = 1
+),
+final as (
+    select
+        player_id,
+        player_firstname,
+        player_lastname,
+        player_name,
+        is_active,
+        position,
+        birth_country,
+        birth_state_province,
+        birth_city,
+        birthdate,
+        draft_year,
+        draft_round,
+        draft_team_id,
+        draft_overall_pick,
+        draft_pick_in_round,
+        current_team_id,
+        team_full_name,
+        shoots_catches,
+        height_centimeters,
+        height_inches,
+        weight_kilogram,
+        weight_pounds
+        from player_info
+        where rn = 1
 )
 
-select distinct * from player_info
+select  * from final
